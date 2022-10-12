8ba8c755a5bafbde6983ac80a0263689327c96d9
HGPlays <81820733+HGPlays@users.noreply.github.com>
2022-10-12 14:28:14 +0200

    first commit

---
+++ Door.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargeStation
+{
+    interface IDoor
+    {
+        void OnDoorOpen();
+
+        void OnDoorClose();
+
+        bool OnDoorState(bool state);
+    }
+
+
+
+    public class Door : IDoor
+    {
+        public void OnDoorOpen()
+        {
+            // Write something to console
+            OnDoorState(true);
+        }
+
+        public void OnDoorClose()
+        {
+
+        }
+
+        public bool OnDoorState(bool state)
+        {
+            return state;
+        }
+    }
+}

10df69f4675b4a9ddac1a8ee587cddab9f80cf05
oscarboe <Oscarboen@gmail.com>
2022-10-26 15:23:27 +0200

    Tried to recreat SWT2_Console

---
+++ Test_Doubles/StubDoor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace SWT2.Stubs
+{
+    public class StubDoor : IDoor
+    {
+
+        StubDoor()
+        {
+
+        }
+        public bool _open { get; set; }
+        public bool locked { get; }
+        public event EventHandler<DoorEventArgs> DoorStateChange;
+        public void DoorOpened()
+        {
+            
+        }
+        public void DoorClosed()
+        {
+
+        }
+        public void DoorUnlock()
+        {
+
+        }
+        public void DoorLock()
+        {
+
+        }
+    }
+}

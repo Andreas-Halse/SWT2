16999a5318705dc83c7ff0f8b3817f22350e9bed
HGPlays <legoandreas@gmail.com>
2022-10-26 09:45:24 +0200

    first test

--- fakeDisplay.cs
+++ fakeDisplay.cs
@@ -3,10 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary;
+
 
 namespace SWT2
 {
-    internal class fakeDisplay
+    internal class fakeDisplay : IDisplay
     {
+        public void Connection()
+        {
+            string connectionString = "Phone is connected";
+            System.Console.WriteLine(connectionString);
+            
+        }
+
+        public void LoadRFID()
+        {
+            string loadRFIDString = "Load your RFID";
+            System.Console.WriteLine(loadRFIDString);
+        }
+
+        public void ConnectionError()
+        {
+            string connectionErrorString = "Phone is NOT connected - an error occurred";
+            System.Console.WriteLine(connectionErrorString);
+        }
+
+        public void Occupied()
+        {
+            string occupiedString = "Charger is occupied";
+            System.Console.WriteLine(occupiedString);
+        }
+
+        public void RFIDError()
+        {
+            string RFIDErrorString = "RFID not loaded - an error occurred";
+            System.Console.WriteLine(RFIDErrorString);
+        }
+
+        public void RemovePhone()
+        {
+            string removePhoneString = "Please remove phone";
+            System.Console.WriteLine(removePhoneString);
+        }
+
+        public void FullyCharged()
+        {
+            string fullyChargedString = "Phone is fully charged";
+            System.Console.WriteLine(fullyChargedString);
+        }
+
+        public void PhoneCharging()
+        {
+            string phoneChargingString = "Phone is charging";
+            System.Console.WriteLine(phoneChargingString);
+        }
+
+        public void ChargeError()
+        {
+            string chargeErrorString = "Phone is NOT Charging - an error occurred";
+            System.Console.WriteLine(chargeErrorString);
+        }
+
     }
+
 }
+
